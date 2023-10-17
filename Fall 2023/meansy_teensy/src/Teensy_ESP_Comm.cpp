685e9cf386dfebc6e583455108b8d8e41a32d0ca
J4mZzy <yzhao1@ufl.edu>
2023-10-17 17:27:04 -0400

    Created teensyComms class for communication functions

--- Fall 2023/meansy_teensy/src/Teensy_ESP_Comm.cpp
+++ Fall 2023/meansy_teensy/src/Teensy_ESP_Comm.cpp
@@ -2,23 +2,30 @@
 
 
 // Initialize UDP IP address and port for ESP
-void initUDP(String address, String port)
+void TeensyComms::initUDP(String address, String port)
 {
     Serial1.print("I"); // Identify init
     Serial1.print(address); // Send IP address
     Serial1.print(port); // Identify init
     Serial1.print("#"); // Deliminator
-} 
+}
 
-void initSubscriber(String topicName, messageType topicType, void* callbackFunctionPtr) // Send information regarding ros2 topics to subscribe to ESP
+TeensyComms::TeensyComms()
 {
+    num_sub = 0; 
+}
+
+void TeensyComms::initSubscriber(String topicName, messageType topicType, void *callbackFunctionPtr) // Send information regarding ros2 topics to subscribe to ESP
+{
+    num_sub += 1; 
     Serial1.print("S");
+    Serial1.print(num_sub); // send number of subscriptions
     Serial1.print(topicName); // send topic name
-
-
-
-
+    Serial1.print(topicType); // Send message type 
 }
 
 
-void publish(String topicName, String topicType, int topicValue, int publishFrequency); // Send information regarding ros2 topics to publish to ESP
+void TeensyComms::publish(String topicName, String topicType, int topicValue, int publishFrequency) // Send information regarding ros2 topics to publish to ESP
+{
+
+} 
--- Fall 2023/meansy_teensy/src/Teensy_ESP_Comm.h
+++ Fall 2023/meansy_teensy/src/Teensy_ESP_Comm.h
@@ -10,11 +10,19 @@
 #pragma once
 
 #include <Arduino.h>
+#include <map>
 
 
 enum messageType {msgFloat64Multiarray, msgBool, msgString, msgFloat64};
 
+class TeensyComms{
 
-void initUDP(String address, String port); // Initialize UDP IP address and port for ESP
-void initSubscriber(String topicName, messageType topicType, void* callbackFunctionPtr); // Send information regarding ros2 topics to subscribe to ESP
-void publish(String topicName, String topicType, int topicValue, int publishFrequency); // Send information regarding ros2 topics to publish to ESP
+    int num_sub; 
+    std::map<String, void*> subbed_topics;
+
+    public: 
+        TeensyComms();
+        void initUDP(String address, String port); // Initialize UDP IP address and port for ESP
+        void initSubscriber(String topicName, messageType topicType, void* callbackFunctionPtr); // Send information regarding ros2 topics to subscribe to ESP
+        void publish(String topicName, String topicType, int topicValue, int publishFrequency); // Send information regarding ros2 topics to publish to ESP
+};
