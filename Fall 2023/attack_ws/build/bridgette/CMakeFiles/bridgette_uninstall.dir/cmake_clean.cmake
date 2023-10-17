c6915ddde19d18369a7ca5a39558c5c4874a0212
J4mZzy <yzhao1@ufl.edu>
2023-10-17 17:13:52 -0400

    Created ros2 python package for bridge

--- Fall 2023/attack_ws/build/bridgette/CMakeFiles/bridgette_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bridgette_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bridgette_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Fall 2023/attack_ws/install/bridgette/share/bridgette/package.sh
+++ Fall 2023/attack_ws/install/bridgette/share/bridgette/package.sh
@@ -78,8 +78,8 @@ _colcon_package_sh_source_script() {
 }
 
 # source sh hooks
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/bridgette/hook/cmake_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/bridgette/local_setup.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/bridgette/hook/pythonpath.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/bridgette/hook/ament_prefix_path.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
